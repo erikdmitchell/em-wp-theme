ac5c3536b3e52a1a7efa7ac919154fb57a3fe959
Erik <erikdmitchell@gmail.com>
2020-12-10 15:38:35 -0500

    init

---
+++ 404.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found)
+ *
+ * @package WordPress
+ * @subpackage emdotnet
+ * @since emdotnet 1.0.0
+ */
+?>
+<?php get_header(); ?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+    <header class="entry-header">
+        <h1 class="page-title"><?php _e( 'Not Found', 'emdotnet' ); ?></h1>
+    </header><!-- .entry-header -->
+
+    <div class="entry-content">
+        <h2><?php _e( "This is somewhat embarrassing, isn't it?", 'emdotnet' ); ?></h2>
+        <p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'emdotnet' ); ?></p>
+
+        <?php get_search_form(); ?>
+    </div><!-- .entry-content -->
+
+</article><!-- #post-## -->
+
+<?php
+get_footer();
---
+++ readme.txt
@@ -0,0 +1,43 @@
+=== em-wp-theme ===
+Contributors: Erik Mitchell
+Tags: white, 
+Requires at least: 3.5
+Tested up to: 5.5.3
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A basic theme for WordPress.
+
+== Description ==
+
+A basic theme for WordPress.
+
+It's a good starting point to be built upon.
+
+== Installation ==
+
+1. Upload `em-wp-theme` to the `/wp-content/theme/` directory
+2. Activate the theme through the 'Themes' (Appearance) menu in WordPress
+
+== Frequently Asked Questions ==
+
+Coming soon...
+
+== Screenshots ==
+
+Coming soon...
+
+== Hooks and Filters ==
+
+Coming soon...
+
+== Changelog ==
+
+= 1.0.0 =
+
+ * Initial setup and theme build out.
+
+== Upgrade Notice ==
+
+None yet.
