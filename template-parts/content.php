9927c2d335ef84058dbd2598b2e8d1d3e1d6ded5
Erik <erikdmitchell@gmail.com>
2020-12-14 13:49:31 -0500

    added featured image

--- template-parts/content.php
+++ template-parts/content.php
@@ -11,6 +11,7 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <?php emdotnet_theme_post_thumbnail(); ?>
     <header class="entry-header">
         <?php
         if ( is_single() ) :
